30e732f1e6eca6f005a4d6dae2ecced684c3de24
PTT\danhhn <danhhn@phuthotech.vn>
2023-03-10 16:44:52 +0700

    - Int

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { RoleComponent } from './role/role.component';
+import { RestaurantComponent } from './restaurant/restaurant.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    RoleComponent,
+    RestaurantComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/models/restaurent.model.ts
@@ -0,0 +1,10 @@
+export interface Restaurent{
+    id: number;
+    name: string;
+    descreption: string;
+    phone: string;
+    address: string;
+    created: Date;
+    updated: Date;
+    deleted: boolean;
+}
